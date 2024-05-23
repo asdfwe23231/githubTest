c58a9a6c470c558d910ca98d6afaf3a979a4bbeb
githubTest <166200860+asdfwe23231@users.noreply.github.com>
2024-05-23 18:27:54 +0800

    构造器

--- git_test/src/main/java/com/zlt/User.java
+++ git_test/src/main/java/com/zlt/User.java
@@ -3,4 +3,12 @@ package com.zlt;
 public class User {
 	String username;
 	int age;
+
+	public User() {
+	}
+
+	public User(String username, int age) {
+		this.username = username;
+		this.age = age;
+	}
 }
